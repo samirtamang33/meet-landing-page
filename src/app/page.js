70dab45eddc71e6e93f321d45cd534dc2401079f
Samir <samiirtamang@gmail.com>
2025-07-01 14:16:46 +0545

    setup project-complete-nav-hero-section

--- src/app/page.js
+++ src/app/page.js
@@ -3,92 +3,69 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div className={styles.main}>
+      <nav className={styles.nav}>
+        {/* Navigation content */}
         <Image
+          src="/assets/logo.svg"
+          alt="Meet Logo"
+          width={118}
+          height={28}
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      </nav>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      {/* Hero section content */}
+      <div className={styles.hero}>
+        <Image
+          src="/assets/tablet/image-hero.png"
+          alt="Hero Image"
+          width={443}
+          height={160}
+          className={styles.heroImage}
+        />
+
+        <div className={styles.heroText}>
+          <div className={styles.heroTitle}>Group Chat for Everyone</div>
+          <div className={styles.heroSubtitle}>
+            Meet makes it easy to connect with others face-to-face virtually and
+            collaborate across any device.
+          </div>
         </div>
-      </main>
+      </div>
+
+      {/* Main content */}
+      <div className={styles.mainContent}>
+        <div className={styles.imageGrid}>
+          <Image
+            src="/assets/desktop/image-woman-in-videocall.jpg"
+            alt="Hero Image"
+            width={147.5}
+            height={144}
+          />
+          <Image
+            src="/assets/desktop/image-woman-in-videocall.jpg"
+            alt="Hero Image"
+            width={147.5}
+            height={144}
+          />
+          <Image
+            src="/assets/desktop/image-woman-in-videocall.jpg"
+            alt="Hero Image"
+            width={147.5}
+            height={144}
+          />
+          <Image
+            src="/assets/desktop/image-woman-in-videocall.jpg"
+            alt="Hero Image"
+            width={147.5}
+            height={144}
+          />
+        </div>
+      </div>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        {/* Footer content */}
+        <p>© 2025 Meet. All rights reserved.</p>
       </footer>
     </div>
   );
